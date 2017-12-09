7f0324607e2af270dc059c174be321b8ff449624
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2017-12-09 16:58:46 -0500

    Create Users registration and login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    set_user
+
+    session[:user_id] = @user.id if @user
+
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    reset_session
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(session_params)
+  end
+
+  def session_params
+    params.require(:session).permit(:dni, :supply_code)
+  end
+end
